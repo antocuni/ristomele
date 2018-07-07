e334ba4bc39245635e1924fc866ca81162d6fefc
Antonio Cuni <anto.cuni@gmail.com>
2018-07-07 11:37:53 +0200

    autogenerate the button grid representing the tables

--- mobile/src/ristomele/gui/tables.py
+++ mobile/src/ristomele/gui/tables.py
@@ -1,4 +1,17 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 
 class Tables(Screen):
-    pass
+
+    ROWS = 7
+    COLS = 3
+
+    def __init__(self, **kwds):
+        super(Tables, self).__init__(**kwds)
+        self.grid = GridLayout(rows=self.ROWS, cols=self.COLS)
+        self.ids.layout.add_widget(self.grid)
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                tname = '%s%s' % (row+1, col+1)
+                self.grid.add_widget(Button(text=tname))
