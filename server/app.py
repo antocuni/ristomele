55388183886f8f45bc0733fa137bc29f7252ac29
Antonio Cuni <anto.cuni@gmail.com>
2018-07-22 04:57:00 +0200

    this is needed to load templates from server/templates instead of from the root dir

--- server/app.py
+++ server/app.py
@@ -7,11 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug import secure_filename
 from server import config
 
-app = flask.Flask('risto_server')
+app = flask.Flask('risto_server', root_path='server')
 STATIC = config.ROOT.join('server', 'static')
 MOBILE = config.ROOT.join('mobile')
 DB = config.ROOT.join('db.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % DB
+
 db = SQLAlchemy(app)
 from server import model
 db.create_all()
