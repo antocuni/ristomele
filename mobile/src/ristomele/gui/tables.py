88788a9ab256517865b28458f5fa1320922e14b5
Antonio Cuni <anto.cuni@gmail.com>
2018-08-09 01:59:51 +0200

    complete refactor of the UI: now we have a simple text box and we update the waiter name when we click on a table

--- mobile/src/ristomele/gui/tables.py
+++ mobile/src/ristomele/gui/tables.py
@@ -4,16 +4,14 @@ from kivy.uix.spinner import Spinner
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
 from ristomele.gui.uix import FlatButton, MyScreen
+from ristomele import model
+
 
 class TableButton(FlatButton):
     table = ObjectProperty()
 
     ## def on_release(self):
     ##     self.table.busy = not self.table.busy
-
-
-class TableSpinner(Spinner):
-    pass
 
 
 class BaseTablesScreen(MyScreen):
@@ -26,38 +24,25 @@ class BaseTablesScreen(MyScreen):
         self.ids.layout.add_widget(self.grid)
         for t in self.restaurant.tables:
             self.grid.add_widget(self.make_widget(t))
-        self.update_waiters()
+
 
 class TablesScreen(BaseTablesScreen):
-
-    def update_waiters(self):
-        pass
 
     def make_widget(self, table):
         return TableButton(table=table)
 
 
-class MySpinnerOption(FlatButton):
-    pass
+
+class EditableTable(FlatButton):
+    table = ObjectProperty(default=model.Table())
+
 
 class EditTablesScreen(BaseTablesScreen):
 
-    def update_waiters(self):
-        waiters = ('anto', 'pippo', 'pluto', '...')
-        for spinner in self.grid.walk():
-            spinner.values = waiters
-
     def make_widget(self, table):
-        def on_text(spinner, text):
-            # make sure to show also the table name when we click on a button
-            if text.startswith(table.name):
-                return
-            spinner.text = '%s\n%s' % (table.name, text)
-        #
-        spinner = Spinner(
-            text=table.name,
-            values=(),
-            option_cls=MySpinnerOption,
-        )
-        spinner.bind(text=on_text)
-        return spinner
+        w = EditableTable(table=table,
+                          on_release=self.update_waiter)
+        return w
+
+    def update_waiter(self, editable_table):
+        editable_table.table.waiter = self.ids.waiter_name.text
