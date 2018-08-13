7e2d7934b8c288927b58009e72b06a975a53a563
Antonio Cuni <anto.cuni@gmail.com>
2018-08-13 02:07:51 +0000

    turn the spooler into a systemd service, and write a small script to install the services

--- server/spooler.py
+++ server/spooler.py
@@ -1,5 +1,5 @@
 """
-Usage: spooler/main.py SPOOLDIR [options]
+Usage: spooler SPOOLDIR [options]
 
 Options:
 
@@ -12,20 +12,36 @@ import time
 import traceback
 import py
 import docopt
+import logging
+from server import config
+LOGFILE = config.ROOT.join('log', 'spooler.log')
+
+def setup_logging():
+    logging.root.setLevel(logging.INFO)
+    #
+    h1 = logging.StreamHandler(sys.stdout)
+    h1.setLevel(logging.INFO)
+    logging.root.addHandler(h1)
+    #
+    h2 = logging.FileHandler(str(LOGFILE))
+    h2.setLevel(logging.INFO)
+    logging.root.addHandler(h2)
 
 
 def main():
+    setup_logging()
     args = docopt.docopt(__doc__)
     spooldir = py.path.local(args['SPOOLDIR'])
     printer = py.path.local(args['--printer'])
     #
     if spooldir.check(dir=False):
-        print '%s is not a valid directory' % spooldir
+        logging.error('%s is not a valid directory', spooldir)
         return 1
     if printer.check(exists=False):
-        print 'The printer device %s does not exists' % printer
+        logging.error('The printer device %s does not exists', printer)
         return 1
     #
+    logging.info('spooler starting')
     while True:
         for txt in spooldir.listdir('*.txt'):
             print_document(txt, printer)
@@ -33,15 +49,14 @@ def main():
 
 def print_document(txt, printer):
     try:
-        print 'Printing', txt.basename, '...',
+        logging.info('Printing %s', txt.basename)
         content = txt.read()
         with printer.open('wb') as f:
             f.write(content)
         txt.remove()
-        print 'DONE'
+        logging.info('DONE')
     except:
-        print 'ERROR!'
-        traceback.print_exc()
+        logging.exception('ERROR!')
 
 if __name__ == '__main__':
     sys.exit(main())
