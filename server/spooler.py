1d58a6dabf9b8dc369faca9e0d48fd53faed1d38
Antonio Cuni <anto.cuni@gmail.com>
2018-08-13 02:24:06 +0000

    don't check arguments at startup: the printer could come later, as well as the dir

--- server/spooler.py
+++ server/spooler.py
@@ -35,29 +35,30 @@ def main():
     spooldir = py.path.local(args['SPOOLDIR'])
     printer = py.path.local(args['--printer'])
     #
-    if spooldir.check(dir=False):
-        logging.error('%s is not a valid directory', spooldir)
-        return 1
-    if printer.check(exists=False):
-        logging.error('The printer device %s does not exists', printer)
-        return 1
-    #
-    logging.info('spooler starting')
+    logging.info('Spooler starting')
+    orders_dir = spooldir.join('orders').ensure(dir=True)
+    drinks_dir = spooldir.join('drinks').ensure(dir=True)
     while True:
-        for txt in spooldir.listdir('*.txt'):
-            print_document(txt, printer)
+        logging.info('beat')
+        print_orders(orders_dir)
+        print_drinks(drinks_dir, printer)
         time.sleep(1)
 
-def print_document(txt, printer):
+def print_orders(orders_dir):
+    pass
+
+def print_drinks(d, printer):
     try:
-        logging.info('Printing %s', txt.basename)
-        content = txt.read()
-        with printer.open('wb') as f:
-            f.write(content)
-        txt.remove()
-        logging.info('DONE')
+        for txt in d.listdir('*.txt'):
+            logging.info('Printing %s', txt.basename)
+            content = txt.read()
+            with printer.open('wb') as f:
+                f.write(content)
+            txt.remove()
+            logging.info('DONE')
     except:
         logging.exception('ERROR!')
+
 
 if __name__ == '__main__':
     sys.exit(main())
