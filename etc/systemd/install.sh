3712f1a0f4e045c072b9ae35745298f95c3afc90
Antonio Cuni <anto.cuni@gmail.com>
2024-08-13 13:47:37 +0200

    make sure to install both services

--- etc/systemd/install.sh
+++ etc/systemd/install.sh
@@ -3,6 +3,6 @@ sudo cp *.service /etc/systemd/system/
 # now in theory we could do "systemctl enable ristomele", to automatically
 # start at at boot, but it doesn't work. Instead, we do it manually
 
-sudo ln -fs /etc/systemd/system/ristomele-*.service /etc/systemd/system/multi-user.target.wants/
+sudo ln -fs /etc/systemd/system/ristomele*.service /etc/systemd/system/multi-user.target.wants/
 
 sudo systemctl daemon-reload
