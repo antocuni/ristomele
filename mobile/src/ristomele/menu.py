694c61108fd94d3a0066c0485d9bbe45b95a9bc7
Antonio Cuni <anto.cuni@gmail.com>
2025-06-05 02:02:49 +0200

    new menu

--- mobile/src/ristomele/menu.py
+++ mobile/src/ristomele/menu.py
@@ -6,9 +6,88 @@ Foc = lambda name, price: model.MenuItem(name='Foc. '+ name, price=price)
 Sep = lambda name: model.MenuItem(kind='separator', name=name)
 
 def get_menu():
-    return menu_13_agosto()
+    #return menu_13_agosto()
     #return menu_14_agosto()
-    #return menu_sagra()
+    return menu_sagra()
+
+def menu_sagra():
+    return [
+        Sep('Focaccini'),
+        Foc('Zeneize de Me', 2),
+        Foc('Boscaiolo e cotto', 5.5),
+        Foc('Boscaiolo e crudo', 5.5),
+        Foc('Boscaiolo e speck', 5.5),
+        Foc('Brie e miele', 4),
+        Foc('Cotto', 4),
+        Foc('Cotto e pistacchio', 5),
+        Foc('Cotto e stracchino', 5),
+        Foc('Cotto, stracchino e pistacchio', 6),
+        Foc('Crema di carciofi', 4),
+        Foc('Crema di carciofi e cotto', 5),
+        Foc('Crema di carciofi e crudo', 5),
+        Foc('Crema di carciofi e salame', 5),
+        Foc('Crema di carciofi e salame piccante', 5),
+        Foc('Crema di carciofi e speck', 5),
+        Foc('Crudo', 4),
+        Foc('Crudo e pistacchio', 5),
+        Foc('Crudo e stracchino', 5),
+        Foc('Crudo, stracchino e pistacchio', 6),
+        Foc('Gorgonzola', 4),
+        Foc('Gorgonzola e noci', 5),
+        Foc('Gorgonzola e pistacchio', 5),
+        Foc('Gorgonzola, miele e noci', 5),
+        Foc('Pesto', 4),
+        Foc('Pesto e stracchino', 5),
+        Foc('Porchetta', 5),
+        Foc('Porchetta e cipolle', 5.5),
+        Foc('Porchetta e pistacchio', 6),
+        Foc('Salame', 4),
+        Foc('Salame piccante', 4),
+        Foc('Salame piccante e gorgo', 5),
+        Foc('Salame picc. e stracchino', 5),
+        Foc('Salsiccia', 5),
+        Foc('Salsiccia e cipolle', 5.5),
+        Foc('Speck', 4),
+        Foc('Speck e brie', 5),
+        Foc('Speck e pistacchio', 5),
+        Foc('Speck, brie e pistacchio', 6),
+        Foc('Stracchino', 4),
+        Foc('Stracchino e pistacchio', 5),
+        Foc('Crema al Pistacchio', 5),
+        Foc('Nutella', 4),
+
+        Sep('Focaccini Speciali del Giorno'),
+        Foc('VENERDI - Pancetta affumicata e peperoni grigliati', 6.5),
+        Foc('SABATO - Mortadella, salsa al pistacchio e burratina', 6.5),
+        Foc('DOMENICA - Fesa di tacchino e salsa tonnata', 6.5),
+
+        Sep('Alla piastra'),
+        Item(name='Salsiccia', price=4.5),
+        Item(name='Salsiccia e patatine', price=5.5),
+        Item(name='Porchetta', price=4.5),
+        Item(name='Porchetta e patatine', price=5.5),
+        Item(name='Patate fritte', price=3),
+
+        Sep('BAR'),
+        Drink('ECO-Bicchiere', 1),
+        Drink('Bicchiere Piccolo Vino', 1.5),
+        Drink('Bicchiere Grande Vino', 3),
+        Drink('Birra artigianale PILS', 5),
+        Drink('Birra artigianale WEISS', 5),
+        Drink('Sangria', 5),
+        Drink('Spritz', 5),
+        Drink('Acqua minerale 0.50L', 1),
+        Drink('Coca Cola', 2),
+        Drink('Fanta', 2),
+        Drink('Gazzosa', 2),
+        Drink('THE Limone', 2),
+        Drink('THE Pesca', 2),
+        Drink('Amaro Camatti', 3),
+        Drink('Liquore Liquirizzia', 3),
+        Drink('Caffe', 1),
+    ]
+
+
 
 def menu_13_agosto():
     """
